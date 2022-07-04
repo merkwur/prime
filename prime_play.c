ebeec0f07f45dda1eec91181e16dc5dac5e43eb8
merkwur <semihiyikalender@gmail.com>
2022-07-04 15:30:45 +0300

    commit

--- prime_play.c
+++ prime_play.c
@@ -2,34 +2,34 @@
 #include <math.h>
 
 int is_prime(int n) {
-  int a, i;
-  a = floor(sqrt(n)) + 1;
+    int a, i;
+    a = floor(sqrt(n)) + 1;
 
-  for (i = 2; i < a; i++){
-    if (n % i == 0){
-      return 0;
+    for (i = 2; i < a; i++){
+        if (n % i == 0){
+            return 0;
+        }
     }
-  }
-  return 1;
+    return 1;
 }
 
 int main(){
-  int j, k, x, c, mod;
+    int j, k, x, c, mod;
   
-  k = 1000;
-  mod = 12;
+    k = 1000;
+    mod = 12;
 
-  int diff[k];
+    int diff[k];
 
-  for (j = 1; j < k; j++) {
-    x = mod * j;
-    c = x;
-    while (is_prime(c) == 0) {
-      c++;
-    }
-    diff[j] = (c-x);
-    printf("%u\n", diff[j]);
-  } 
-  return 0;
+    for (j = 1; j < k; j++) {
+        x = mod * j;
+        c = x;
+        while (is_prime(c) == 0) {
+            c++;
+        }
+        diff[j] = (c-x);
+        printf("%u\n", diff[j]);
+    } 
+    return 0;
 }
 
